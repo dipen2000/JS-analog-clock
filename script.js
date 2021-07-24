0bcd9d0decbe4fb0ec808521992f839bfdce9551
dipen2000 <dipenchavda2000@gmail.com>
2021-07-24 15:07:03 +0530

    modified : Made the second hand smooth and changed the alignment of some numbers

--- script.js
+++ script.js
@@ -5,13 +5,24 @@ for(let i=1;i<=11;i++){
     angle+=30;
 }
 
-setInterval(setClock , 1000);
+setInterval(setClock , 1);
 
 const hourHand = document.querySelector(".hour-hand");
 const minuteHand = document.querySelector(".minute-hand");
 const secondHand = document.querySelector(".second-hand");
+const dateForMili = new Date();
+const exactMiliSecond = dateForMili.getMilliseconds();
+let miliseconds = exactMiliSecond;
 
 function setClock(){
+    if(miliseconds < 60000){
+        miliseconds++;
+    }
+    else{
+        miliseconds = 0;
+    }
+    let miliSecondratio = miliseconds / 60000;
+    let miliSecondRotation = miliSecondratio * 360;
     let currentDate = new Date();
     let secondratio = (currentDate.getSeconds()) / 60; 
     let secondRotation = secondratio*360;
@@ -20,9 +31,9 @@ function setClock(){
     let hourratio = (minuteratio + currentDate.getHours())/12;
     let hourRotation = hourratio*360;
     // let minuteRotation = (currentDate.getSeconds())
-    secondHand.style.setProperty("--rotation" , `${secondRotation}deg`);
-    secondHand.style.setProperty("transform" , `rotate(${secondRotation}deg)`);
-    rotateHand(secondHand , secondRotation);
+    // secondHand.style.setProperty("--rotation" , `${miliSecondRotation}deg`);
+    // secondHand.style.setProperty("transform" , `rotate(${secondRotation}deg)`);
+    rotateHand(secondHand , miliSecondRotation);
     rotateHand(minuteHand , minuteRotation);
     rotateHand(hourHand , hourRotation);
 }
@@ -32,3 +43,16 @@ function rotateHand(hand , rotation){
     hand.style.setProperty("transform" , `rotate(${rotation}deg)`);
 }
 setClock();
+
+for(let i=4;i<=8;i++){
+    let element = document.querySelector(`.number${i} div`);
+    element.style.setProperty("transform" , `rotate(180deg)`);
+}
+
+for(let i=3;i<=9;i+=6){
+    if(i==9){
+        let element = document.querySelector(`.number${i} div`);
+        element.style.setProperty("transform" , `rotate(90deg)`);
+    }
+    
+}
