a2af969ae51b4a2284efd5de4efa04ea670b56c7
dipen2000 <dipenchavda2000@gmail.com>
2021-07-24 13:35:11 +0530

    complete : analog clock

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-let angle = 0;
-for(let i=1;i<=12;i++){
+let angle = 30;
+for(let i=1;i<=11;i++){
     let ele = document.querySelector(`.number${i}`);
     ele.style.setProperty("--rotation" , `${angle}deg`);
     angle+=30;
 }
+
+setInterval(setClock , 1000);
+
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const secondHand = document.querySelector(".second-hand");
+
+function setClock(){
+    let currentDate = new Date();
+    let secondratio = (currentDate.getSeconds()) / 60; 
+    let secondRotation = secondratio*360;
+    let minuteratio = (secondratio + currentDate.getMinutes())/60;
+    let minuteRotation = (minuteratio)*360;
+    let hourratio = (minuteratio + currentDate.getHours())/12;
+    let hourRotation = hourratio*360;
+    // let minuteRotation = (currentDate.getSeconds())
+    secondHand.style.setProperty("--rotation" , `${secondRotation}deg`);
+    secondHand.style.setProperty("transform" , `rotate(${secondRotation}deg)`);
+    rotateHand(secondHand , secondRotation);
+    rotateHand(minuteHand , minuteRotation);
+    rotateHand(hourHand , hourRotation);
+}
+
+function rotateHand(hand , rotation){
+    hand.style.setProperty("--rotation" , `${rotation}deg`);
+    hand.style.setProperty("transform" , `rotate(${rotation}deg)`);
+}
+setClock();
